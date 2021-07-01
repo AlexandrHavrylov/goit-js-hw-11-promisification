16dd2713b814e346533c305ca76c003956619ea5
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-07-01 22:18:57 +0300

    решение дз 11

--- src/index.js
+++ src/index.js
@@ -1,33 +1,126 @@
-import colors from './colors.json'
+/* ЗАДАНИЕ 1
+
+Напиши функцию delay(ms), которая возвращает промис, 
+переходящий в состояние "resolved" через ms миллисекунд.
+ Значением исполнившегося промиса должно быть
+ то кол-во миллисекунд которое передали во время вызова функции delay.*/
+
+
+const delay = ms => {
+  return new Promise((resolve) => {
+ setTimeout(() => {
+  resolve (ms)
+    }, ms)
+
+  })
+   
+}
+const logg = time => console.log(`Resolved after ${time}ms`);
+// Вызовы функции для проверки
+delay(2000).then(logg); // Resolved after 2000ms
+delay(1000).then(logg); // Resolved after 1000ms
+delay(1500).then(logg); // Resolved after 1500ms
+
+
+/* ЗАДАНИЕ 2
+
+Перепиши функцию toggleUserState() так, чтобы она
+ не использовала callback-функцию 
+callback, а принимала всего два параметра
+ allUsers и userName и возвращала промис.*/
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = new Promise((resolve) =>
+    resolve(
+      allUsers.map(user => user.name === userName ? { ...user, active: !user.active } : user,)
+    ))
+
+  return updatedUsers
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
 
 
 
-const body = document.querySelector('body')
-const startBtn = document.querySelector('button[data-action="start"]')
-const stopBtn = document.querySelector('button[data-action="stop"]')
 
-const randomIntegerFromInterval = (min, max) => {
+
+
+/* ЗАДАНИЕ 3
+
+Перепиши функцию makeTransaction() так, чтобы она 
+не использовала callback-функции onSuccess
+ и onError, а принимала всего один параметр 
+ transaction и возвращала промис..*/
+
+ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
 
-let changeColor = null;
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const canProcess = Math.random() > 0.3;
 
-startBtn.addEventListener('click', onStartBtnClick)
+    if (canProcess) {
+   resolve(transaction.id, delay);
+    } 
+      reject(transaction.id);
+    
+  }, delay);
+})
+}   
 
-function onStartBtnClick() {
-  startBtn.setAttribute('disabled', 'disabled')
-   changeColor =  setInterval(() => {
-    const randomColor = colors[randomIntegerFromInterval(0, colors.length)]
-    body.style.backgroundColor = randomColor;
-  }, 1000);
-  
-}
+const logSuccess = (id, time) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
 
-stopBtn.addEventListener('click', onStopBtnClick)
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
 
-function onStopBtnClick() {
+/*
+ * Работает так
+ */
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
 
-  clearInterval(changeColor)
-  startBtn.removeAttribute('disabled', 'disabled')
-}
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
